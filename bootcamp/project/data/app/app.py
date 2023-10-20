f430921e80d4a4ddac876f368446a042493931d9
Hélio Bomfim de Macêdo Filho <heliomacedofilho@gmail.com>
2023-10-20 12:08:02 -0300

    Update app.py

--- bootcamp/project/data/app/app.py
+++ bootcamp/project/data/app/app.py
@@ -8,8 +8,9 @@ def load_data():
                'Dispersão': {'Região': None, 'Mesorregião': None, 'Microrregião': None, 'Município': None}}
     de_para = {'choropleth': 'Coroplético', 'regiao': 'Região', 'mesorregiao': 'Mesorregião',
                'microrregiao': 'Microrregião', 'municipio': 'Município', 'scatter_geo': 'Dispersão'}
-    pickle_filenames = ['choropleth-regiao.pkl', 'choropleth-mesorregiao.pkl',
-                        'choropleth-municipio.pkl', 'scatter_geo-regiao.pkl', 'scatter_geo-mesorregiao.pkl']
+    pickle_filenames = ['choropleth-regiao.pkl', 'choropleth-mesorregiao.pkl', 'choropleth-microrregiao.pkl',
+                        'choropleth-municipio.pkl', 'scatter_geo-regiao.pkl', 'scatter_geo-mesorregiao.pkl', 
+                        'scatter_geo-microrregiao.pkl', 'scatter_geo-municipio.pkl']
     url = 'https://github.com/heliomacedofilho/bootcamp-analise-de-dados-enap-2023/raw/main/bootcamp/project/data/app/'
     for filename in pickle_filenames:
         type_of_map, intraregion = filename.rstrip('.pkl').split('-')
@@ -23,5 +24,5 @@ st.markdown("---")
 type_of_map = st.sidebar.selectbox('Qual o tipo de mapa a representar os dados?',
                                    ('Coroplético', 'Dispersão'))
 intraregion = st.selectbox('Qual a malha geográfica do Brasil a ser considerada?',
-                          ('Região', 'Mesorregião'))
+                          ('Região', 'Mesorregião', 'Microrregião'))
 st.plotly_chart(figures[type_of_map][intraregion]);
